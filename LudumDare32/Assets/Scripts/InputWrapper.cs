cdee277b5fb624cc10674f0bcf44399e38c186bd
Luke Nizik <elejdor@gmail.com>
2015-04-18 11:52:40 +0200

    [+]  InputWrapper [+] movement script

---
+++ LudumDare32/Assets/Scripts/InputWrapper.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputWrapper : MonoBehaviour {
+	
+	private float horizontalAxis;
+	private float verticalAxis;
+	private static InputWrapper instance;
+	
+	public static InputWrapper Instance
+	{
+		get
+		{
+			return Instance;
+		}
+	}
+	
+	void Awake()
+	{
+			instance = this;
+	}
+	
+	// Use this for initialization
+	void Start () {
+		horizontalAxis = 0;
+		verticalAxis = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		horizontalAxis = Input.GetAxisRaw("Horizontal");
+		verticalAxis = Input.GetAxisRaw("Vertical");
+	}
+	
+	#region InputWrappers
+	public float HorizontalAxis
+	{
+		get
+		{
+			return horizontalAxis;
+		}
+	}
+	
+	public float VerticalAxis
+	{
+		get
+		{
+			return verticalAxis;
+		}
+	}
+	
+	public float Accelerate
+	{
+		get
+		{
+			if (Input.GetKey(KeyCode.Space))
+			{
+				return 1.0f;
+			}
+			
+			return 0;
+		}
+	}
+	#endregion
+}
---
+++ LudumDare32/Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMovement : MonoBehaviour {
+
+	Vector3 forceDirection = Vector3.zero;
+	float maxForce;
+	float desiredForce;
+	float horizontalForce;
+	float verticalForce;
+	InputWrapper iw;
+	
+	
+	Rigidbody rb;
+	
+	// Use this for initialization
+	void Start () {
+		rb = this.gameObject.GetComponent<Rigidbody>();
+		iw = InputWrapper.Instance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void HandleMovment()
+	{
+		horizontalForce = iw.HorizontalAxis;
+		verticalForce = iw.VerticalAxis;
+		
+		forceDirection.x = horizontalForce;
+		forceDirection.y = verticalForce;
+		forceDirection.Normalize();
+		
+		rb.AddForce(forceDirection * (verticalForce + horizontalForce));
+	}
+}
