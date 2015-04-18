f420aa6b2d4e58e0f4a6e25a163e90e361fe953e
piechota <piechota33@gmail.com>
2015-04-18 16:31:23 +0200

    [+] kinect

---
+++ LudumDare32/Assets/Scripts/Kinect/BodyManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Windows.Kinect;
+
+public class BodyManager : MonoBehaviour {
+
+    private KinectSensor _sensor;
+    private BodyFrameReader _bodyFrameReader;
+    private Body[] _bodyData;
+
+    public Body[] bodyData { get { return _bodyData; } }
+    public bool isKinect { get { return _sensor != null; } }
+    
+	// Use this for initialization
+    void Awake()
+    {
+        _sensor = KinectSensor.GetDefault();
+        if (_sensor != null)
+        {
+            _bodyFrameReader = _sensor.BodyFrameSource.OpenReader();
+
+            if (!_sensor.IsOpen)
+                _sensor.Open();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(_bodyFrameReader != null)
+        {
+            using (BodyFrame frame = _bodyFrameReader.AcquireLatestFrame())
+            {
+                if (frame != null)
+                {
+                    if (_bodyData == null) _bodyData = new Body[_sensor.BodyFrameSource.BodyCount];
+
+                    frame.GetAndRefreshBodyData(_bodyData);
+                }
+            }
+        }
+	}
+    void OnApplicationQuit()
+    {
+        if (_bodyFrameReader != null)
+        {
+            _bodyFrameReader.Dispose();
+            _bodyFrameReader = null;
+        }
+
+        if (_sensor != null)
+        {
+            if (_sensor.IsOpen)
+            {
+                _sensor.Close();
+            }
+
+            _sensor = null;
+        }
+    }
+}
