d3d25b2c53958bd0ea7d2c7afa609f691463687b
Luke Nizik <elejdor@gmail.com>
2015-04-18 19:18:52 +0200

    [+] explosive cubes

---
+++ LudumDare32/Assets/Scripts/Explosion.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+
+    float desiredTimeScale = 1.0f;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            StartCoroutine(Explode(col.transform.position));
+
+            desiredTimeScale = 0.4f;
+
+            
+            StartCoroutine(NormalTime());
+        }
+
+    }
+
+    IEnumerator NormalTime()
+    {
+        yield return new WaitForSeconds(0.5f * Time.timeScale);
+        desiredTimeScale = 1.0f;
+        
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Time.timeScale = Mathf.Lerp(Time.timeScale, desiredTimeScale, 0.1f);
+        Time.fixedDeltaTime = 0.01f * Time.timeScale;
+	}
+
+    IEnumerator Explode(Vector3 position)
+    {
+        yield return new WaitForEndOfFrame();
+        for (int i = 0; i < transform.childCount; i++)
+        {            
+            transform.GetChild(i).GetComponent<Rigidbody>().AddExplosionForce(0.008f, position, 6.0f, 5.0f);
+        }
+    }
+}
--- LudumDare32/Assets/Scripts/GenerateVoxelBlock.cs
+++ LudumDare32/Assets/Scripts/GenerateVoxelBlock.cs
@@ -3,13 +3,59 @@ using System.Collections;
 
 public class GenerateVoxelBlock : MonoBehaviour {
 
+    [SerializeField]
+    int x;
+
+    [SerializeField]
+    int y;
+
+    [SerializeField]
+    int z;
+
+    [SerializeField]
+    float mass;
+    float size;
+
+    [SerializeField]
+    GameObject prefab;
+
 	// Use this for initialization
 	void Start () {
-	
+        size = prefab.transform.localScale.x;
+        GameObject tmpGo;
+
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                for (int k = 0; k < z; k++)
+                {
+                    if ((k == z - 1 || k == 0) || (i == x - 1 || i == 0))
+                    {
+                        tmpGo = (GameObject)GameObject.Instantiate(prefab);
+                        tmpGo.transform.parent = this.gameObject.transform;
+                        tmpGo.transform.localPosition = new Vector3(i * size - size, j * size - size, k * size - size);
+                        tmpGo.GetComponent<Rigidbody>().mass = mass;
+                        //if (j % 2 != 0)
+                        //{
+                        //    tmpGo.layer = LayerMask.NameToLayer("Brothers");
+                        //}
+                        //else
+                        //{
+                        //    tmpGo.layer = LayerMask.NameToLayer("Brothers");
+                        //}
+                    }
+
+                }
+
+                
+            }
+        }
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
 }
